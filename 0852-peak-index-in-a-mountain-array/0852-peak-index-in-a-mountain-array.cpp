361cc19f49015725e379128e9fe6a18d40927614
piyushrj9260-glitch <piyushrj9260@gmail.com>
2026-06-25 15:42:29 +0530

    Time: 0 ms (100%), Space: 63.6 MB (48.24%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s = 1;
+        int e = arr.size() - 2;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        return 0;
+    }
+};
