8357e80c1234a4e5d7598c32313c2f3b92ce1731
piyushrj9260-glitch <piyushrj9260@gmail.com>
2026-06-25 15:32:05 +0530

    Time: 0 ms (100%), Space: 17.9 MB (23.02%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int prefix = 1;
+        int suffix = 1;
+        int ans = nums[0];
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n - i - 1];
+
+            ans = max(ans, max(prefix, suffix));
+        }
+
+        return ans;
+    }
+};
