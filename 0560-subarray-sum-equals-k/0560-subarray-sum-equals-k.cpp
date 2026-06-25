43d19bf7734e89733367b2599520cd783c655269
piyushrj9260-glitch <piyushrj9260@gmail.com>
2026-06-25 16:46:39 +0530

    Time: 51 ms (43.4%), Space: 51.1 MB (8.89%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,15 +3,14 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
         mp[0]=1;
-        int sum =0;
+        int preSum =0;
         int cnt=0;
 
         for(int i=0;i<nums.size();i++){
-            sum += nums[i];
-            if(mp.find(sum - k) != mp.end()){
-                cnt += mp[sum-k];
-            }
-            mp[sum]++;
+            preSum += nums[i];
+            int remove = preSum - k;
+            cnt += mp[remove];
+            mp[preSum] += 1;
         }
         return cnt;
     }
