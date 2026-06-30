0cfbb9d6ac9bea2ebbb80e3da263b331400f190e
piyushrj9260-glitch <piyushrj9260@gmail.com>
2026-06-30 20:47:09 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.89%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int cnt=0;
+        int i=n-1;
+        while(i>=0 && s[i] == ' '){
+            cnt = 0;
+            i--;
+        }
+
+        while(i>=0 && s[i] != ' '){
+            cnt++;
+            i--;
+        }
+        return cnt;
+    }
+};
