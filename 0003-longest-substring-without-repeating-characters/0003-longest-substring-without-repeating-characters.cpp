59ba9160d03fac9e7c477443ffd19f556b93c0cc
Piyush Raj <piyushrj9260@gmail.com>
2026-09-19 15:57:48 +0530

    Time: 59 ms (43.66%), Space: 19.4 MB (41.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,21 +5,18 @@ public:
         int maxLen = 0;
         int l = 0;
         int r = 0;
-        unordered_map<char,int>mp;
+        unordered_map<char, int> mp;
 
-        while(r<n){
-            if(mp.find(s[r]) == mp.end()){
+        while (r < n) {
+            if (mp.find(s[r]) == mp.end()) {
+                mp[s[r]] = r;
+            } else if (mp.find(s[r]) != mp.end()) {
+                if (mp[s[r]] >= l) {
+                    l = mp[s[r]] + 1;
+                }
                 mp[s[r]] = r;
             }
-            else if(mp.find(s[r]) != mp.end()){
-                if(mp[s[r]] >= l){
-                    l = mp[s[r]] + 1;
-                    mp[s[r]] = r;
-                }else{
-                    mp[s[r]] = r;
-                }
-            }
-            maxLen = max(maxLen,r-l+1);
+            maxLen = max(maxLen, r - l + 1);
             r++;
         }
         return maxLen;
